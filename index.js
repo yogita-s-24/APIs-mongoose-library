3e10077e6a4238c87520fc04f5ac2a118b9b3ade
yogitashete2412 <yogitashete24@gmail.com>
2023-06-10 22:21:58 +0530

    ref : index.js file

--- index.js
+++ index.js
@@ -5,76 +5,73 @@ import dotnv from 'dotenv';
 dotnv.config();
 
 const app = express();
- app.use(express.json());
+app.use(express.json());
 
 
-async function connectMongoDB(){
+async function connectMongoDB() {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    if(conn)
-    {
+    if (conn) {
         console.log('Mongo DB Connected');
     }
-    else
-    {
+    else {
         console.log('Error');
     }
 }
 connectMongoDB();
 
-app.post("/student", async(req,res)=>{
+app.post("/student", async (req, res) => {
     const fullName = req.body.fullName;
     const email = req.body.email;
-    const regNo = req.body.regNo ;
+    const regNo = req.body.regNo;
 
     const newStud = new Student({
-      fullName : fullName,
-      email : email,
-      regNo : regNo 
+        fullName: fullName,
+        email: email,
+        regNo: regNo
     })
 
     const savedStudent = await newStud.save();
 
     res.json({
-        success :true,
-        message:'Student Saved Successfully',
-        data : savedStudent
+        success: true,
+        message: 'Student Saved Successfully',
+        data: savedStudent
     })
 })
 
-app.get('/students', async(req, res)=>{
+app.get('/students', async (req, res) => {
     const students = await Student.find();
 
     res.json({
-        success:true,
-        message:'Students fetched Successfully',
-        data:students
+        success: true,
+        message: 'Students fetched Successfully',
+        data: students
     })
 })
 
-app.get('/student',async(req, res)=>{
+app.get('/student', async (req, res) => {
     const email = req.query.email;
-  
-    const student = await Student.findOne({email : email})
-  
-    if(student)
-    {
-      return res.json({
-        success: true,
-        message: "Student fetched successfully",
-        data: student
-      })
-    }
-   else{
-       res.json({
-         success: false,
-         message: "Student not found",
-         data: null
-       })
-   }
-  
-  })
 
-app.listen(5000,()=>{
+    const student = await Student.findOne({ email: email })
+
+    if (student) {
+        return res.json({
+            success: true,
+            message: "Student fetched successfully",
+            data: student
+        })
+    }
+    else {
+        res.json({
+            success: false,
+            message: "Student not found",
+            data: null
+        })
+    }
+
+})
+
+app.listen(5000, () => {
     console.log('Listen on port 5000');
 })
 
